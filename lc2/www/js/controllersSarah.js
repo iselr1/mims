dbc52bee3130c9d71b106b58df3f48607f4405b3
sarahmele <sarah.mele@students.bfh.ch>
2016-10-25 15:13:00 +0200

    Labyrinth zeichnen
    erste Punkte des Labyrinths

--- lc2/www/js/controllersSarah.js
+++ lc2/www/js/controllersSarah.js
@@ -26,17 +26,50 @@ angular.module('starter.controllersSarah', [])
 
 .controller('RouteCtrl', function($scope, $stateParams) {
 
- var $configCircle = {
-  id : "example1",
-  radius : "70",
-  fillStyle : 'red',
-  fill : 'true',
-  lineWidth : 5,
-  strokeStyle : 'yellow'
-};
+// Set up to draw the labyrinth!
+var my_canvas = document.getElementById("labyrinth");
+var ctx = my_canvas.getContext("2d");
 
- //var example = new DrawJS($configCircle);
-//example.circle();
+
+
+// Draw the points
+ctx.beginPath();
+ctx.arc(50, 400, 15, 0, 2*Math.PI);
+ctx.closePath();
+ctx.lineWidth = 5;
+ctx.stroke();
+ctx.fillStyle = "black";
+
+
+//draw a line
+ctx.beginPath();
+ctx.moveTo(100,400);
+ctx.lineTo(200, 200);
+ctx.stroke();
+
+// Draw the left eye
+context.beginPath();
+context.arc(75, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the right eye
+context.beginPath();
+context.arc(114, 75, 5, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+
+// Draw the mouth
+context.beginPath();
+context.arc(95, 90, 26, Math.PI, 2*Math.PI, true);
+context.closePath();
+context.fill();
+
+// Write "Hello, World!"
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);
+
+
 
 })
 ;
