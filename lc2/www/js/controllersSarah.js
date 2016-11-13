c6b54fb3ebecd4909f0396cb5793212acb2f94e6
sarahmele <sarah.mele@students.bfh.ch>
2016-11-13 13:55:37 +0100

    Funktion Labyrinth
    Labyrinth zeichnen in einer Funktion

--- lc2/www/js/controllersSarah.js
+++ lc2/www/js/controllersSarah.js
@@ -28,653 +28,163 @@ angular.module('starter.controllersSarah', [])
 
 .controller('RouteCtrl', function($scope, $stateParams) {
 
+var xfactor = 0.8;
+var yfactor = 0.8;
+
+var xvaluefrom = 0;
+var yvaluefrom = 0;
+var xvalueto = 0;
+var yvalueto = 0;
+var xvalue = 0;
+var yvalue = 0;
+
+var line1;
+var line2;
+var line3;
+var line4;
+var line5;
+var line6;
+var line7;
+var line8;
+var line9;
+var line10;
+var line11;
+var line12;
+var line13;
+var line14;
+var line15;
+var line16;
+var line17;
+var line18;
+var line19;
+var line20;
+var line21;
+var line22;
+var line23;
+var line24;
+var line25;
+var line26;
+var line27;
+var line28;
+var line29;
+var line30;
+var line31;
+var line32;
+var line33;
+var line34;
+var line35;
+var line36;
+
+var point1;
+var point2;
+var point3;
+var point4;
+var point5;
+var point6;
+var point7;
+var point8;
+var point9;
+var point10;
+var point11;
+var point12;
+var point13;
+var point14;
+var point15;
+var point16;
+var point17;
+var point18;
+var point19;
+var point20;
+var point21;
+var point22;
+
 my_canvas = document.getElementById("labyrinth");
 ctx = my_canvas.getContext("2d");
 
-/*--  Draw the lines --*/
-//Draw Line 1
-ctx.beginPath();
-ctx.moveTo(51,682); //von
-ctx.lineTo(160, 415); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 2
-ctx.beginPath();
-ctx.moveTo(160, 415); //von
-ctx.lineTo(109, 238); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 3
-ctx.beginPath();
-ctx.moveTo(109, 238); //von
-ctx.lineTo(160, 142); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 4
-ctx.beginPath();
-ctx.moveTo(160, 142); //von
-ctx.lineTo(294.5, 104); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 5
-ctx.beginPath();
-ctx.moveTo(294.5,104); //von
-ctx.lineTo(326.5, 396.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 6
-ctx.beginPath();
-ctx.moveTo(109,238); //von
-ctx.lineTo(326.5, 396); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 7
-ctx.beginPath();
-ctx.moveTo(160,416); //von
-ctx.lineTo(326.5, 396); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 8
-ctx.beginPath();
-ctx.moveTo(160,416); //von
-ctx.lineTo(224, 606); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 9
-ctx.beginPath();
-ctx.moveTo(51,682); //von
-ctx.lineTo(224, 606); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 10
-ctx.beginPath();
-ctx.moveTo(224, 606); //von
-ctx.lineTo(371, 628); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 11
-ctx.beginPath();
-ctx.moveTo(371, 628); //von
-ctx.lineTo(326,396); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 12
-ctx.beginPath();
-ctx.moveTo(326,396); //von
-ctx.lineTo(512, 472.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 13
-ctx.beginPath();
-ctx.moveTo(512, 472.5); //von
-ctx.lineTo(422.5, 320); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 14
-ctx.beginPath();
-ctx.moveTo(422.5, 320); //von
-ctx.lineTo(441.5, 180.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 15
-ctx.beginPath();
-ctx.moveTo(441.5, 180.5); //von
-ctx.lineTo(294.5, 104); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 16
-ctx.beginPath();
-ctx.moveTo(441.5, 180.5); //von
-ctx.lineTo(627,110.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 17
-ctx.beginPath();
-ctx.moveTo(441.5, 180.5); //von
-ctx.lineTo(544, 275.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 18
-ctx.beginPath();
-ctx.moveTo(544, 275.5); //von
-ctx.lineTo(761.5, 339); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 19
-ctx.beginPath();
-ctx.moveTo(761.5, 339); //von
-ctx.lineTo(627,110.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 20
-ctx.beginPath();
-ctx.moveTo(761.5, 339); //von
-ctx.lineTo(710.4, 472.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 21
-ctx.beginPath();
-ctx.moveTo(710.4, 472.5); //von
-ctx.lineTo(512, 472.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 22
-ctx.beginPath();
-ctx.moveTo(512, 472.5); //von
-ctx.lineTo(678.5, 663); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 23
-ctx.beginPath();
-ctx.moveTo(678.5, 663); //von
-ctx.lineTo(371,628); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 24
-ctx.beginPath();
-ctx.moveTo(678.5, 663); //von
-ctx.lineTo(806.5, 606); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 25
-ctx.beginPath();
-ctx.moveTo(806.5, 606); //von
-ctx.lineTo(710.4, 472.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 26
-ctx.beginPath();
-ctx.moveTo(710.4, 472.5); //von
-ctx.lineTo(928,402.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 27
-ctx.beginPath();
-ctx.moveTo(928,402.5); //von
-ctx.lineTo(761.5, 339); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 28
-ctx.beginPath();
-ctx.moveTo(928,402.5); //von
-ctx.lineTo(889.5, 263); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 29
-ctx.beginPath();
-ctx.moveTo(889.5, 263); //von
-ctx.lineTo(877,91.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 30
-ctx.beginPath();
-ctx.moveTo(889.5, 263); //von
-ctx.lineTo(761.5, 339); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 31
-ctx.beginPath();
-ctx.moveTo(877,91.5); //von
-ctx.lineTo(627,110.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 32
-ctx.beginPath();
-ctx.moveTo(928,402.5); //von
-ctx.lineTo(909,555); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 33
-ctx.beginPath();
-ctx.moveTo(909,555); //von
-ctx.lineTo(806.5, 606); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 34
-ctx.beginPath();
-ctx.moveTo(909,555); //von
-ctx.lineTo(953.5, 669.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 35
-ctx.beginPath();
-ctx.moveTo(953.5, 669.5); //von
-ctx.lineTo(806.5, 606); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-//Draw Line 36
-ctx.beginPath();
-ctx.moveTo(422.5, 320); //von
-ctx.lineTo(544, 275.5); //zu
-ctx.closePath();
-ctx.lineWidth = 2;
-ctx.stroke();
-
-
-/*--  Draw the Points --*/
-//Draw point 1 START
-ctx.beginPath();
-ctx.arc(51, 682, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 2
-ctx.beginPath();
-ctx.arc(160, 415.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 3
-ctx.beginPath();
-ctx.arc(109, 237.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 4
-ctx.beginPath();
-ctx.arc(160, 142.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 5
-ctx.beginPath();
-ctx.arc(294.5, 104, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 6
-ctx.beginPath();
-ctx.arc(326.5, 396.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 7
-ctx.beginPath();
-ctx.arc(224, 606, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 8
-ctx.beginPath();
-ctx.arc(371, 628, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 9
-ctx.beginPath();
-ctx.arc(678.5, 663, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 10
-ctx.beginPath();
-ctx.arc(512, 472.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 11
-ctx.beginPath();
-ctx.arc(422.5, 320, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 12
-ctx.beginPath();
-ctx.arc(441.5, 180.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 13
-ctx.beginPath();
-ctx.arc(544, 275.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 14
-ctx.beginPath();
-ctx.arc(627, 110.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 15
-ctx.beginPath();
-ctx.arc(761.5, 339, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 16
-ctx.beginPath();
-ctx.arc(710.5, 472.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 17
-ctx.beginPath();
-ctx.arc(806.5, 606, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 18
-ctx.beginPath();
-ctx.arc(909, 555, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 19
-ctx.beginPath();
-ctx.arc(928, 402.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 20
-ctx.beginPath();
-ctx.arc(889.5, 263, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 21
-ctx.beginPath();
-ctx.arc(877, 91.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-//Draw point 22 END
-ctx.beginPath();
-ctx.arc(953.5, 669.5, 15, 0, 2*Math.PI);
-ctx.fillStyle = "white";
-ctx.fill();
-ctx.closePath();
-ctx.lineWidth = 5;
-ctx.fillStyle = "black";
-ctx.stroke();
-
-
-
-
-
-// Set up to draw the labyrinth!
-/*var Mydraw = function (config){
-  $config = config;
-  my_canvas = document.getElementById("labyrinth");
-  ctx = my_canvas.getContext("2d");
-}
-
-Mydraw.circle = function(){
-  var centerX = $config['centerX'];
-  var centerY = $config['centerY'];
-  var radius = $config['radius'];
-
+  drawLine = function(xvaluefrom, yvaluefrom, xvalueto, yvalueto) {
   ctx.beginPath();
-  ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  ctx.fillStyle = $config['fillStyle'];
-  if ($config['fill'] !== undefined){
-    ctx.fill();
-  }
-  ctx.lineWidth = $config['lineWidth'];
-  ctx.strokeStyle = $config['strokeStyle'];
-  ctx.stroke();
-};
-
-var $configCircle = {
-  radius: "15",
-  fillStyle: '',
-  fill: 'false',
-  lineWidth: 5,
-  strokeStyle: 'black'
-};
-var circle1 = new Mydraw($configCircle);
-circle1.circle();*/
-
-
-/*var $configCircle = {
-  id: "example1",
-  radius: "70",
-  fillStyle: 'red',
-  fill: 'true',
-  lineWidth: 5,
-  strokeStyle: 'yellow'
-};
-var example1 = new DrawJS($configCircle);
-example1.circle();
-
-DrawJS.prototype.rectangle = function(){
-       ctx.beginPath();
-       var width = c.width;
-       var height = c.height;
-
-       ctx.rect(10, 10, width, height);
-       ctx.fillStyle = $config['fillStyle'];
-
-       if ($config['fill'] !== undefined){
-         ctx.fill();
-       }
-
-       ctx.lineWidth = $config['lineWidth'];
-       ctx.strokeStyle = $config['strokeStyle'];
-       ctx.stroke();
-};
-
-DrawJS.prototype.line = function(){
-
-        ctx.beginPath();
-        ctx.moveTo($config['moveToX'], $config['moveToY']);
-        ctx.lineTo($config['lineToX'], $config['lineToY']);
-        ctx.lineWidth = $config['lineWidth'];
-
-        // set line color
-        ctx.strokeStyle = $config['strokeStyle'];
-        ctx.stroke();
-};
-
-
-DrawJS.prototype.text = function(){
-       var x = c.width / 2;
-       var y = c.height / 2;
-
-       ctx.font = $config['font'];
-       // textAlign aligns text horizontally relative to placement
-       ctx.textAlign = $config['textAlign'];
-       // textBaseline aligns text vertically relative to font style
-       ctx.textBaseline = $config['textBaseline'];
-       ctx.fillStyle = $config['fillStyle'];
-
-       if ($config['fill'] !== undefined){
-         ctx.fill();
-       }
-       ctx.fillText($config['text'], x, y);
-};
-
-
-DrawJS.prototype.drawImage = function(){
-        var x = 0;
-        var y = 0;
-        var imageObj = new Image();
-        imageObj.onload = function() {
-          ctx.drawImage(imageObj, x, y);
-        };
-        imageObj.src = $config['src'];
-
-};
-
-
-
-DrawJS.prototype.triangle = function(){
-  ctx.beginPath();
-
-  ctx.moveTo($config['moveToX'], $config['moveToY']);
-
-  ctx.lineTo($config['lineToX'], $config['lineToY']);
-  ctx.lineTo($config['lineToY'], $config['lineToX']);
-
+  ctx.moveTo(xvaluefrom*xfactor,yvaluefrom*yfactor); //von
+  ctx.lineTo(xvalueto*xfactor, yvalueto*yfactor); //zu
   ctx.closePath();
-
-  // the outline
-  ctx.lineWidth = $config['lineWidth'];
-  ctx.strokeStyle = $config['strokeStyle'];
+  ctx.lineWidth = 3;
   ctx.stroke();
+  };
 
-  // the fill color
-  ctx.fillStyle = $config['fillStyle'];
-  if ($config['fill'] !== undefined){
+  drawPoint = function(xvalue, yvalue) {
+    ctx.beginPath();
+    ctx.arc(xvalue*xfactor, yvalue*yfactor, 18, 0, 2*Math.PI);
+    ctx.fillStyle = "white";
     ctx.fill();
-  }
+    ctx.closePath();
+    ctx.lineWidth = 5;
+    ctx.fillStyle = "black";
+    ctx.stroke();
+  };
 
-};*/
+  $scope.drawLab = function(){
+    console.info("inside Lab");
+    line1 = drawLine(51, 682, 160, 415);
+    line2 = drawLine(160, 415, 109, 238);
+    line3 = drawLine(109, 238, 160, 142);
+    line4 = drawLine(160, 142, 294.5, 104);
+    line5 = drawLine(294.5, 104, 326.5, 396.5);
+    line6 = drawLine(109, 238, 326.5, 396.5);
+    line7 = drawLine(160, 416, 326.5, 396.5);
+    line8 = drawLine(160, 416, 224, 606);
+    line9 = drawLine(51, 682, 224, 606);
+    line10 = drawLine(224, 606, 371, 628);
+    line11 = drawLine(371, 628, 326, 396);
+    line12 = drawLine(326, 396, 512, 472.5);
+    line13 = drawLine(512, 472.5, 422.5, 320);
+    line14 = drawLine(422.5, 320, 441.5, 180.5);
+    line15 = drawLine(441.5, 180.5, 294.5, 104);
+    line16 = drawLine(441.5, 180.5, 627, 110.5);
+    line17 = drawLine(441.5, 180.5, 544, 275.5);
+    line18 = drawLine(544, 275.5, 761.5, 339);
+    line19 = drawLine(761.5, 339, 627,  110.5);
+    line20 = drawLine(761.5,  339, 710.4, 472.5);
+    line21 = drawLine(710.4,  472.5, 512, 472.5);
+    line22 = drawLine(512, 472.5, 678.5, 663);
+    line23 = drawLine(678.5, 663, 371, 628);
+    line24 = drawLine(678.5, 663, 806.5, 606);
+    line25 = drawLine(806.5, 606, 710.5, 472.5);
+    line26 = drawLine(710.5, 472.5, 928, 402.5);
+    line27 = drawLine(928, 402.5, 761.5, 339);
+    line28 = drawLine(928, 402.5, 889.5, 263);
+    line29 = drawLine(889.5, 263, 877, 91.5);
+    line30 = drawLine(889.5, 263, 761.5, 339);
+    line31 = drawLine(877, 91.5, 627, 110.5);
+    line32 = drawLine(928,  402.5, 909, 555);
+    line33 = drawLine(909, 555, 806.5, 606);
+    line34 = drawLine(909, 555, 953.5, 669.5);
+    line35 = drawLine(953.5, 669.5, 806.5, 606);
+    line36 = drawLine(422.5,  320, 544, 275.5);
 
+    point1 = drawPoint(51, 682);
+    point2 = drawPoint(160, 415.5);
+    point3 = drawPoint(109,  237.5);
+    point4 = drawPoint(160, 142.5);
+    point5 = drawPoint(294.5, 104);
+    point6 = drawPoint(326.5, 396.5);
+    point7 = drawPoint(224, 606);
+    point8 = drawPoint(371, 628);
+    point9 = drawPoint(678.5, 663);
+    point10 = drawPoint(512, 472.5);
+    point11 = drawPoint(422.5, 320);
+    point12 = drawPoint(441.5, 180.5);
+    point13 = drawPoint(544, 275.5);
+    point14 = drawPoint(627, 110.5);
+    point15 = drawPoint(761.5, 339);
+    point16 = drawPoint(710.5, 472.5);
+    point17 = drawPoint(806.5, 606);
+    point18 = drawPoint(909, 555);
+    point19 = drawPoint(928, 402.5);
+    point20 = drawPoint(889.5, 263);
+    point21 = drawPoint(877, 91.5);
+    point22 = drawPoint(953.5, 669.5);
+  };
+
+  $scope.drawLab();
 
 })
 ;
